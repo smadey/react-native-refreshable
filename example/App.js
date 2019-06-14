0f25fd9774a64a5f3e6986041a5d31636fa847b8
Zhang Chengrong <zhangchengrong@yuewen.com>
2019-06-14 18:08:01 +0800

    feat(example): use expo

--- example/App.js
+++ example/App.js
@@ -7,26 +7,19 @@ import {
   View,
 } from 'react-native'
 
-import Refreshable, { createRenderRefresher } from '..'
+import Refreshable, { useSpinner } from '..'
 
-const renderRefresher = createRenderRefresher({
-  spinner: {
-    duration: 1000,
-    render(props) {
-      return (
-        <Animated.Image
-          style={{ width: 16, height: 16, transform: [{ rotate: props.progress.interpolate({inputRange: [0, 100], outputRange: ['0deg', '360deg']}) }] }}
-          source={{uri: 'https://readx-her-1252317822.image.myqcloud.com/boss/5322_spinner.png'}}
-          resizeMethod="scale"
-        />
-      )
-    },
-  },
-  // text: {
-  //   render({ statusTxt }) {
-  //     return <Text style={{color: 'red'}}>{statusTxt}</Text>
-  //   },
-  // },
+const RefreshComponent = React.memo(({ status }) => {
+  const progress = useSpinner(status === 2 || status === 3)
+  return (
+    <View style={{ alignItems: 'center', flexDirection: 'row', height: 60, justifyContent: 'center' }}>
+      <Animated.Image
+        resizeMethod="scale"
+        source={{ uri: 'https://readx-her-1252317822.image.myqcloud.com/boss/5322_spinner.png' }}
+        style={{ height: 16, width: 16, transform: [{ rotate: progress.interpolate({ inputRange: [0, 100], outputRange: ['0deg', '360deg'] }) }] }}
+      />
+    </View>
+  )
 })
 
 function App() {
@@ -52,7 +45,7 @@ function App() {
         ref={refreshable}
         style={styles.refreshable}
         scrollEnabled={enabled}
-        renderRefresher={renderRefresher}
+        RefreshComponent={RefreshComponent}
         onRefresh={onRefresh}
       >
         <Button title={`toggle(${enabled !== false ? 'enabled' : 'disabled'})`} onPress={onToggle} />
